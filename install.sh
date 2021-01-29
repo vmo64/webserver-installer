b0f85f932cdacbd43269028ca7a8ddcda252eba6
TechWinYT <45152843+vmo64@users.noreply.github.com>
2021-01-29 22:51:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,28 +2,28 @@
 
 
     echo  "LAMP Stack Install Script"
-    output "Contact me on discord NGX#6969 if you have any questions"
+    echo  "Contact me on discord NGX#6969 if you have any questions"
 
-    output "Do not run this script on a system with already installed/broken lamp stack or it will fail!"
-    output "Continuing in 5 seconds...."
+    echo "Do not run this script on a system with already installed/broken lamp stack or it will fail!"
+    echo "Continuing in 5 seconds...."
     sleep 5
 
     if [ "$EUID" -ne 0 ]; then
-        output "Please run this script as root."
+        echo "Please run this script as root."
         
     fi
 
-output "Automatic architecture detection initialized..."
+echo "Automatic architecture detection initialized..."
     MACHINE_TYPE=`uname -m`
     if [ ${MACHINE_TYPE} == 'x86_64' ]; then
-        output "64-bit server detected! OK."
-        output ""
+        echo "64-bit server detected! OK."
+        echo ""
     else
-        output "Unsupported architecture detected! Please switch to 64-bit."
+        echo "Unsupported architecture detected! Please switch to 64-bit."
         
     fi
 
-        output "Good to go"
+        echo "Good to go"
         sleep 5
         
 

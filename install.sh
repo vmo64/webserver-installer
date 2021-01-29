72b64ef2b662ef1525989581e4f857727aaf87f5
TechWinYT <45152843+vmo64@users.noreply.github.com>
2021-01-29 22:48:16 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 
-preflight(){
-    output "LAMP Stack Install Script"
+
+    echo  "LAMP Stack Install Script"
     output "Contact me on discord NGX#6969 if you have any questions"
 
     output "Do not run this script on a system with already installed/broken lamp stack or it will fail!"
     output "Continuing in 5 seconds...."
     sleep 5
-    
-    os_check
 
     if [ "$EUID" -ne 0 ]; then
         output "Please run this script as root."
-        exit 3
+        
     fi
 
 output "Automatic architecture detection initialized..."
@@ -22,6 +20,12 @@ output "Automatic architecture detection initialized..."
         output ""
     else
         output "Unsupported architecture detected! Please switch to 64-bit."
-        exit 4
-    
-    esac
+        
+    fi
+
+        output "Good to go"
+        sleep 5
+        
+
+
+apt install nano

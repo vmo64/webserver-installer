4056e5301117f5ed834ec5583130d1edf862acf8
TechWinYT <45152843+vmo64@users.noreply.github.com>
2021-01-30 20:53:47 +0100

    Normal fully automatic installer

--- install.sh
+++ install.sh
@@ -1,78 +1,116 @@
 #!/bin/sh
-clear
 echo "###################################################"
-echo  "LAMP Stack install Script"
-echo  "Contact me on discord NGX#6969 if you have any questions"
-echo "Do not run this script on a system with already installed/broken lamp stack or it will fail!"
-echo "Continuing in 5 seconds...."
+echo "Preparing to launch script and installing prerequisites!"
+echo "Please wait!"
 echo "###################################################"
-sleep 5;
+apt update
+apt install -y aptitude
+apt-get -y install expect
+echo " "
+echo "###################################################"
+    echo  "LAMP Stack install Script"
+    echo  "Contact me on discord NGX#6969 if you have any questions"
+    echo "Do not run this script on a system with already installed/broken lamp stack or it will fail!"
+    echo "Continuing in 5 seconds...."
+	echo "###################################################"
+    sleep 5;
+echo " "
 echo "###################################################"
 echo "Updating System!"
 echo "###################################################"
-aptitude update
+aptitude update >> lamp-install.log
+echo " "
 echo "###################################################"
 echo "Installing Apache2 Web Server"
 echo "###################################################"
-aptitude install -y apache2
-systemctl status apache2
-systemctl is-enabled apache2
-clear
+aptitude install -y apache2 >> lamp-install.log
+systemctl status apache2 >> lamp-install.log
+systemctl is-enabled apache2 >> lamp-install.log
+echo " "
 echo "###################################################"
 echo "Installing MariaDB"
 echo "###################################################"
-aptitude install -y mariadb-server
-aptitude install -y mariadb-client
-systemctl status mariadb
-systemctl is-enabled mariadb
-clear
+aptitude install -y mariadb-server >> lamp-install.log
+aptitude install -y mariadb-client >> lamp-install.log
+systemctl status mariadb >> lamp-install.log
+systemctl is-enabled mariadb >> lamp-install.log
+echo " "
 echo "###################################################"
 echo "Configuring MariaDB"
-echo "Please follow this guide!"
-echo "Set a root password? Y"
-echo "Remove anonymous users? Y"
-echo "Disallow root login remotely? Y"
-echo "Remove test database and access to it? Y"
-echo "Reload privilege tables now? Y"
-echo "Continuing in 15 seconds..."
 echo "###################################################"
-sleep 15s;
-mysql_secure_installation
-clear
+[ ! -e /usr/bin/expect ]
+SECURE_MYSQL=$(expect -c "
+
+set timeout 10
+spawn mysql_secure_installation
+
+expect \"Enter current password for root (enter for none): \"
+send \"n\r\"
+expect \"Change the root password? \[Y/n\] \"
+send \"y\r\"
+expect \"New password: \"
+send \"1234567890\r\"
+expect \"Re-enter new password: \"
+send \"1234567890\r\"
+expect \"Remove anonymous users? \[Y/n\] \"
+send \"y\r\"
+expect \"Disallow root login remotely? \[Y/n\] \"
+send \"y\r\"
+expect \"Remove test database and access to it? \[Y/n\] \"
+send \"y\r\"
+expect \"Reload privilege tables now? \[Y/n\] \"
+send \"y\r\"
+expect eof
+")
+echo " "
 echo "###################################################"
 echo "Installing PHP"
 echo "###################################################"
-aptitude install -y php
-aptitude install -y libapache2-mod-php
-aptitude install -y php-mysql
-aptitude-cache search php | grep php-
-aptitude install -y php-redis php-zip
-clear
+aptitude install -y php >> lamp-install.log
+aptitude install -y libapache2-mod-php >> lamp-install.log
+aptitude install -y php-mysql >> lamp-install.log
+aptitude-cache search php | grep php- >> lamp-install.log
+aptitude install -y php-redis php-zip >> lamp-install.log
+echo " "
 echo "###################################################"
 echo "Restarting Apache2 service!"
 echo "###################################################"
-systemctl restart apache2
-clear
+systemctl restart apache2 >> lamp-install.log
+echo " "
 echo "###################################################"
 echo "Installing PhpMyAdmin"
-echo "Please follow this guide!"
-echo "If you select yes you will be prompted there questions."
-echo "Web Server to reconfigure automatically: apache2"
-echo "Configure database for phpmyadmin with dbconfig-common: Yes"
-echo "MySQL application password for phpmyadmin: The password you set while making root password earlier"
 echo "###################################################"
-echo "Continuing in 15 seconds..."
+
+if [ ! -f /etc/phpmyadmin/config.inc.php ];
+then
+MYSQL_ROOT_PASS='1234567890'
+
+  echo "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2" | debconf-set-selections
+  echo "phpmyadmin phpmyadmin/dbconfig-install boolean true" | debconf-set-selections
+  echo "phpmyadmin phpmyadmin/mysql/admin-user string root" | debconf-set-selections
+  echo "phpmyadmin phpmyadmin/mysql/admin-pass password $MYSQL_ROOT_PASS" | debconf-set-selections
+  echo "phpmyadmin phpmyadmin/mysql/app-pass password $MYSQL_ROOT_PASS" |debconf-set-selections
+  echo "phpmyadmin phpmyadmin/app-password-confirm password $MYSQL_ROOT_PASS" | debconf-set-selections
+  
+  aptitude install -y phpmyadmin >> lamp-install.log
+ fi
+sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf >> lamp-install.log
+sudo a2enconf phpmyadmin.conf >> lamp-install.log
+sudo systemctl reload apache2.service >> lamp-install.log
+
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
 echo "###################################################"
-sleep 15s;
-aptitude install -y phpmyadmin
-sudo ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf-available/phpmyadmin.conf
-sudo a2enconf phpmyadmin.conf
-sudo systemctl reload apache2.service
-sleep 2s;
-clear
-echo "###################################################"
-echo "Installation completed!
+echo "Installation completed!"
 echo "HTML folder located under /ver/www/html/"
 echo "PhpMyAdmin is located at http://YOUR_SERVER_IP/phpmyadmin"
 echo "We recommend you reboot the machine if you can!"
+echo "Install log is located in lamp-install.log"
+echo "###################################################"
+echo "MariaDB (MySQL) password: 1234567890"
+echo "PhpMyAdmin username: phpmyadmin"
+echo "PhpMyAdmin password: 1234567890"
 echo "###################################################"

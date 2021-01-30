3af518a5a36119f38cd7f18afd5b0a8692651fbb
TechWinYT <45152843+vmo64@users.noreply.github.com>
2021-01-30 21:11:21 +0100

    Apache Let's Encrypt SSL module script

---
+++ ssl.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+echo "###################################################"
+echo "Preparing to launch script and installing prerequisites!"
+echo "Please wait!"
+echo "###################################################"
+apt update >> x3-ssl.log
+apt install -y certbot >> x3-ssl.log
+sudo apt install -y python3-certbot-apache >> x3-ssl.log
+echo " "
+echo "###################################################"
+echo "Let's Encrypt SSL Script"
+echo  "Contact me on discord NGX#6969 if you have any questions"
+echo "Continuing in 5 seconds...."
+echo "###################################################"
+sleep 5;
+echo " "
+certbot certonly --apache
+echo " "
+echo "###################################################"
+echo "Let's Encrypt SSL is installed!"
+echo "###################################################"
